4d869311aaa20dc518f8d61d172551dbff559e66
hridoymh <mhhridoy0c@gmail.com>
2022-11-03 15:52:52 +0600

    likes app added

--- store/models.py
+++ store/models.py
@@ -65,12 +65,12 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
-    unit_price = models.DecimalField(max_digit=6, decimal_point=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 class CartItem(models.Model):
-    cart = models.ForeignKey(Cart, no_delete=models.CASCADE)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
